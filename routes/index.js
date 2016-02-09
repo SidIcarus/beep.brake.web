74172c3d1c7859caa7ecbef32c150e520c4ebbf3
bbensch <bbensch@live.com>
2016-02-09 13:10:23 -0500

    Mock website UI's

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Beep.Brake.Web' });
+});
+
+router.get('/dataView', function(req, res, next) {
+  res.render('dataView', {title: 'Beep.Brake.Web Home'})
 });
 
 module.exports = router;
