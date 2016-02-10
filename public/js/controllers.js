dcd4e2096ff7dd0d432e64bddca021373e30f21c
Brad Bensch <bbensch@live.com>
2016-02-10 16:28:49 -0500

    Add Head partial to html, remove test item on scope

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,7 +1,5 @@
 angular.module('beep.brake.controllers', []).
 controller('mainCtrl', function($scope) {
-	$scope.test = "Test Text!";
-
 	$scope.submit = function() {
 		$scope.username = '';
 		$scope.password = '';
