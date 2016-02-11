eca3958d244ed8a90e926c24920d60e262efa390
bbensch <bbensch@live.com>
2016-02-11 15:56:11 -0500

    Add sql file

---
+++ resources/dbSetup.sql
@@ -0,0 +1,52 @@
+CREATE TABLE appuser(
+    deviceId text PRIMARY KEY
+);
+
+CREATE TABLE configuration(
+	id serial PRIMARY KEY,
+	warningtime integer
+);
+
+CREATE TABLE event(
+	Id serial PRIMARY KEY,
+	userid text REFERENCES appuser,
+	hardware text,
+	appversion text,
+	osversion text,
+	config integer REFERENCES configuration,
+	eventdate timestamp
+);
+
+CREATE TABLE segment(
+	id serial PRIMARY KEY,
+	eventid integer REFERENCES event,
+	segtime timestamp
+);
+
+CREATE TABLE numsensordata(
+	id serial PRIMARY KEY,
+	segid integer REFERENCES segment,
+	key text,
+	value float8
+);
+
+CREATE TABLE boolsensordata(
+	id serial PRIMARY KEY,
+	segid integer REFERENCES segment,
+	key text,
+	value bool
+);
+
+CREATE TABLE stringsensordata(
+	id serial PRIMARY KEY,
+	segid integer REFERENCES segment,
+	key text,
+	value text
+);
+
+CREATE TABLE user(
+	id serial PRIMARY KEY,
+	username text UNIQUE,
+	password text,
+	role text
+);
