f05cb0d4b787fe19f03a4b44c73ecf22f22831b6
bbensch <bbensch@live.com>
2016-02-12 12:40:48 -0500

    Fix non-unique device add crash

--- routes/api.js
+++ routes/api.js
@@ -4,34 +4,36 @@ var pg = require('pg');
 
 var conString = "postgres://postgres:1234@localhost/postgres";
 
-router.get('/', function(req, res) {
-  console.log("This got hit");
-  res.json({message: "Yeah. API"});
-});
-
+//Stores new Device Id
 router.post('/newDevice', function(req, res) {
-  console.log(req.body);
   if (!req.body.deviceId) {
       return res.status(400).send({"status": "error", "message": "No deviceId provided"})
   }
-
-  var devId = req.body.deviceId;
 
   pg.connect(conString, function(err, client, done) {
     if (err) {
       return console.error('error fetching client from pool', err);
     }
-    
+
+    //Check for uniqueness. If unique, store, otherwise, send 400
     client.query({
-      text   : "INSERT INTO appuser VALUES($1)",
-      name   : "Device-Registration",
-      values : [devId] 
-    });
-
-    done(); 
+      text   : 'SELECT EXISTS(SELECT 1 FROM appuser WHERE deviceid=$1)',
+      name   : "Unique Check",
+      values : [req.body.deviceId]
+    }, function(err, result) {
+      if (result.rows[0].exists) {
+        return res.status(400).send({"message" : "Device already registered"})
+      } else {
+        client.query({
+          text   : "INSERT INTO appuser VALUES($1)",
+          name   : "Device-Registration",
+          values : [req.body.deviceId] 
+        }, function(err, result) {
+          res.status(201).send({"message" : "Device Registered"})
+        });
+      }
+    });  
   });
-
-  res.status(200).send  ({message: "Api Reached"})
 });
 
 router.post('/newEvent', function(req,res) {
@@ -44,7 +46,7 @@ router.post('/newEvent', function(req,res) {
 
   //Parse and store event in db
 
-  res.json({"message": "Success"});
+  res.status(200).send({"message": "Success"});
 });
 
 module.exports = router;
