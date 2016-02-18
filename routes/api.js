507ebde474e0caeee57ec1dd70107660f4599d41
bbensch <bbensch@live.com>
2016-02-18 09:56:03 -0500

    Fix major bug with adding, add error checking

--- routes/api.js
+++ routes/api.js
@@ -56,60 +56,23 @@ router.post('/newEvent', function(req,res) {
 
         //Parse and store event in db
         console.log("Add event");
-        var d = new Date();
-        var n = d.getTime();
-        var u = new Date(n);
-
         client.query({ 
           text   : "INSERT INTO event VALUES (DEFAULT, (SELECT deviceid FROM appuser WHERE deviceid=$1), $2, $3, $4, (SELECT id FROM configuration WHERE id=1), $5) RETURNING id",
           name   : "Event Creation",
-          values : [newEvent.deviceid, newEvent.hardware, newEvent.appversion, newEvent.osversion, u]
+          values : [newEvent.deviceid, newEvent.hardware, newEvent.appversion, newEvent.osversion, new Date(newEvent.eventdate)]
         }, function(err, results) {
-          //console.log(results.rows[0].id);
-          var p = new Date(new Date().getTime());
-          for (var s = 0; s < newEvent.segments.length; s++) {
-            var segment = newEvent.segments[s];
-            client.query({ 
-              text   : "INSERT INTO segment VALUES (DEFAULT, (SELECT id FROM event WHERE id=$1), $2) RETURNING id",
-              name   : "Segment Creation",
-              values : [results.rows[0].id, p/*segment.segtime*/]
-            }, function(err, results) {
-              for (var i = 0; i < segment.data.length; i++) {
-                var segData = segment.data[i];
-                switch(typeof(segData.value)) {
-                  case 'boolean':
-                    console.log("bool");
-                    client.query({
-                      text   : "INSERT INTO boolsensordata VALUES (DEFAULT, (SELECT id FROM segment WHERE id=$1), $2, $3)",
-                      name   : "Bool Sensor Data Creation",
-                      values : [results.rows[0].id, segData.key, segData.value]
-                    })
-                    break;
-                  case 'number':
-                    console.log("NUMBER");
-                    client.query({
-                      text   : "INSERT INTO numsensordata VALUES (DEFAULT, (SELECT id FROM segment WHERE id=$1), $2, $3)",
-                      name   : "Num Sensor Data Creation",
-                      values : [results.rows[0].id, segData.key, segData.value]
-                    })
-                    break;
-                  case 'string':
-                    console.log("STRING");
-                    client.query ({
-                      text   : "INSERT INTO stringsensordata VALUES (DEFAULT, (SELECT id FROM segment WHERE id=$1), $2, $3)",
-                      name   : "String Sensor Data Creation",
-                      values : [results.rows[0].id, segData.key, segData.value]
-                    })
-                    break;
-                  default:
-                    console.log("Invalid type");
-                }
-              }
 
-              done();
-              return res.status(201).send({"message": "Success"});
-            })
+          if (err) {
+            console.log(err);
+            return res.send(400);
           }
+
+          for (var s = 0; s < newEvent.segments.length; s++) {
+            segCreate(results.rows[0].id, newEvent.segments[s]);
+          }
+
+          done();
+          return res.status(201).send({"message": "Success"});
         }) 
       } else {
         done();
@@ -118,6 +81,64 @@ router.post('/newEvent', function(req,res) {
     });
   });
 });
+
+function segCreate(eventid, segment) {
+  pg.connect(conString, function(err, client, done) {
+    client.query({ 
+        text   : "INSERT INTO segment VALUES (DEFAULT, (SELECT id FROM event WHERE id=$1), $2) RETURNING id",
+        name   : "Segment Creation",
+        values : [eventid, new Date(segment.segtime)]
+      }, function(err, results) {
+
+        if (err) {
+          console.log(err);
+          return;
+        }
+
+        console.log("In callback after segment: " + new Date().getTime());
+        for (var i = 0; i < segment.data.length; i++) {
+          switch(typeof(segment.data[i].value)) {
+            case 'boolean':
+              client.query({
+                text   : "INSERT INTO boolsensordata VALUES (DEFAULT, (SELECT id FROM segment WHERE id=$1), $2, $3)",
+                name   : "Bool Sensor Data Creation",
+                values : [results.rows[0].id, segment.data[i].key, segment.data[i].value]
+              }, function(err, results) {
+                if (err) {
+                  console.log(err);
+                }
+              })
+              break;
+            case 'number':
+              client.query({
+                text   : "INSERT INTO numsensordata VALUES (DEFAULT, (SELECT id FROM segment WHERE id=$1), $2, $3)",
+                name   : "Num Sensor Data Creation",
+                values : [results.rows[0].id, segment.data[i].key, segment.data[i].value]
+              }. function(err, results) {
+                if (err) {
+                  console.log(err);
+                }
+              })
+              break;
+            case 'string':
+              client.query ({
+                text   : "INSERT INTO stringsensordata VALUES (DEFAULT, (SELECT id FROM segment WHERE id=$1), $2, $3)",
+                name   : "String Sensor Data Creation",
+                values : [results.rows[0].id, segment.data[i].key, segment.data[i].value]
+              }, function(err, results) {
+                if (err) {
+                  console.log(err);
+                }
+              })
+              break;
+            default:
+              console.log("Invalid type");
+          }
+        }
+      })
+  });
+
+}
   
 router.post('/login', function(req, res) {
   res.status(200).send();
@@ -139,7 +160,7 @@ router.get('/events', function(req, res) {
 router.get('/event/:id', function(req, res) {
   pg.connect(conString, function(err, client, done) {
     client.query ({
-      text   : "SELECT * FROM event INNER JOIN segment ON (event.id = segment.eventid)WHERE event.id=$1",
+      text   : "SELECT * FROM event INNER JOIN segment ON (segment.eventid = event.id)WHERE event.id=$1",
       name   : "Get Event by id",
       values : [req.params.id]
     }, function(err, results) {
